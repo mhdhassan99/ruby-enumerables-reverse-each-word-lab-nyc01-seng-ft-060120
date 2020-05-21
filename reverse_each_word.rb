96d4f803b8b067805a64764883f2c05d3fd2422a
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-21 21:17:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,11 @@ def reverse_each_word(sen)
   words = sen.split(' ')
   
   words.each do |word|
+    rve_word
     i = word.length
     while i > 0 do
-      word[i]
+      rev_word = word[i] 
+      i -= 1
     end
   end
 end
